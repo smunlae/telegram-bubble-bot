fc08445f6767db2e728b73b314c2704332854abb
s30011 <smunlae@gmail.com>
2025-05-29 17:32:38 +0200

    Removed tg channel subscription check

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,37 +1,19 @@
 import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
-from telegram.ext import Application, CommandHandler, ContextTypes
-from telegram.error import BadRequest
+from telegram.ext import Application, CommandHandler
 
 API_TOKEN = os.environ['TELEGRAM_TOKEN']     # ваш токен
 WEBAPP_URL = os.environ['WEBAPP_URL']       # URL вашего Bubble-chart
-CHANNEL_USERNAME = "@giftsBubbles"           # юзернейм канала
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_id = update.effective_user.id
-
-    try:
-        # проверяем статус пользователя в канале
-        member = await context.bot.get_chat_member(CHANNEL_USERNAME, user_id)
-        is_subscribed = member.status in ("member", "administrator", "creator")
-    except BadRequest:
-        # например, бот не является администратором или канал приватный
-        is_subscribed = False
-
-    if not is_subscribed:
-        # если не подписан — просим подписаться
-        await update.message.reply_text(
-            f"Чтобы пользоваться ботом, подпишись на канал {CHANNEL_USERNAME}"
-        )
-        return
-
-    # пользователь подписан — показываем кнопку WebApp
+async def start(update: Update, context):
+    # Создаём кнопку, которая откроет Web App
     button = InlineKeyboardButton(
         text="Gift Bubbles 💠",
         web_app=WebAppInfo(url=WEBAPP_URL)
     )
     keyboard = InlineKeyboardMarkup([[button]])
     
+    # Отправляем сообщение с inline-клавиатурой
     await update.message.reply_text(
         "Нажми кнопку, чтобы посмотреть изменения цен telegram gifts:",
         reply_markup=keyboard
